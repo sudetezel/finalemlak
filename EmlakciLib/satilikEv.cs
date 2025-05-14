25cf4fcbaa6e4db1729ad717941e5ef3ce680c26
suden <suden@LAPTOP-V6VDQSUJ>
2025-05-14 13:47:27 +0300

    Add project files.

---
+++ EmlakciLib/satilikEv.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmlakciLib
+{
+    public class SatilikEv : Ev
+    {
+        public SatilikEv()
+        {
+
+        }
+
+        public SatilikEv(double satisfiyat, int odasayisi, string semt, int katno, double alan) : base(odasayisi, katno, alan, semt)
+        {
+            Satisfiyat = satisfiyat;
+        }
+
+        public double Satisfiyat { get; set; }
+
+
+        public override string EvBilgileri()
+        {
+            return $"{base.EvBilgileri()}\nSatış Fiyat:{this.Satisfiyat}";
+        }
+    }
+}
+
+
+
---
+++ finalemlak/Program.cs
@@ -0,0 +1,26 @@
+﻿using EmlakciLib;
+
+namespace finalemlak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KiralikEv ke = new KiralikEv(500, 600, 3, "Kiralik Ev", 100, 80);
+            KiralikEv ke1 = new KiralikEv(800, 900, 2, "Kiralik Ev", 120, 90);
+            KiralikEv ke2 = new KiralikEv(10000, 10000, 4, "Kiralik Ev", 200, 100);
+
+
+            var se = new SatilikEv(400, 3, "Satılık Ev", 110, 60);
+            var se1 = new SatilikEv(700, 2, "Satılık Ev", 120, 70);
+            var se2 = new SatilikEv(900, 4, "Satılık Ev", 140, 80);
+
+            Console.WriteLine(ke.ToString());
+
+
+
+        }
+    }
+}
+
+
